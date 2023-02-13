ab269ac1ab89478bf9c5164f79c16d02296020f6
Anna Suuronen <annasuuronen@icloud.com>
2023-02-13 18:18:54 +0200

    tehtävät 3.7 ja 3.8 tehty

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
+  ].join(' ')
+}))
+
 
 let persons = [
   { 
